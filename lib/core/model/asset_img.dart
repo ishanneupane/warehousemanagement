2f695d9913d3da2e5e79278ff50276665575ea41
Ishan Neupane <ishanneupane8945@gmail.com>
2024-03-03 16:21:15 +0545

    c

--- lib/core/model/asset_img.dart
+++ lib/core/model/asset_img.dart
@@ -1,1 +1,22 @@
-
+class Assets {
+  static const String aImage = "assets/image/a.png";
+  static const String bImage = "assets/image/b.png";
+  static const String cImage = "assets/image/c.png";
+  static const String dImage = "assets/image/d.png";
+  static const String demandImage = "assets/image/demand.png";
+  static const String eImage = "assets/image/e.png";
+  static const String fImage = "assets/image/f.png";
+  static const String gImage = "assets/image/g.png";
+  static const String hImage = "assets/image/h.png";
+  static const String iImage = "assets/image/i.png";
+  static const String icLauncherImage = "assets/image/ic_launcher.png";
+  static const String jImage = "assets/image/j.png";
+  static const String kImage = "assets/image/k.png";
+  static const String khaiImage = "assets/image/khai.png";
+  static const String lImage = "assets/image/l.png";
+  static const String mImage = "assets/image/m.png";
+  static const String nImage = "assets/image/n.png";
+  static const String oImage = "assets/image/o.png";
+  static const String pImage = "assets/image/p.png";
+  static const String totImage = "assets/image/tot.png";
+}
