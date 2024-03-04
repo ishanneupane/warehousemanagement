226669abb056997098aa9f2449617e3b3f908e98
Saurav <sauravchaulagain39@gmail.com>
2024-03-04 10:32:48 +0545

    login updated

--- lib/src/login/login.dart
+++ lib/src/login/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:warehousemanagement/src/login/login_api.dart';
 import 'package:warehousemanagement/src/login/user_model/user.dart';
+
 import '../homepage/dashboard.dart';
 import '../registration/register_ui.dart';
 
@@ -122,14 +123,42 @@ class _LoginScreenState extends State<LoginScreen> {
                       // Validate the form
                       if (formKey.currentState!.validate()) {
                         // Form is valid, proceed with login
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                Dashboard(), // Assuming Dashboard is the name of your dashboard screen class
-                          ),
-                        );
-                      }
+                        final filterData = data.length > 0;
+                        if (filterData) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  Dashboard(), // Assuming Dashboard is the name of your dashboard screen class
+                            ),
+                          );
+                        } else {
+                          
+                      showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('AlertDialog Title'),
+        content: const SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('This is a demo alert dialog.'),
+              Text('Would you like to approve of this message?'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Approve'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
                     },
                     child: Text(
                       'Login',
